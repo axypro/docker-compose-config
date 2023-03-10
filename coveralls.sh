796e3b8473485b1a8e76d2a9f0d932dbfaebefc7
Oleg Grigoriev <go.vasac@gmail.com>
2023-03-10 19:29:30 +0400

    Github action - tests - upload coveralls

--- coveralls.sh
+++
@@ -1,5 +0,0 @@
-./vendor/bin/phpunit -c phpunit.xml.dist --coverage-clover clover.xml &&
-composer require php-coveralls/php-coveralls --dev --prefer-dist &&
-echo "json_path: coveralls-upload.json" > .coveralls.yml &&
-echo "coverage_clover: clover.xml" >> .coveralls.yml &&
-./vendor/bin/php-coveralls -c .coveralls.yml
